3c6803564b666420a28427979e62e9d1af6193f2
Somen Samanta <111171599+S0M3N@users.noreply.github.com>
2023-08-04 14:20:53 +0530

    Add files via upload

---
+++ src/pages/TechKranti.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutTechkranti from '../components/AboutTechkranti'
+import TechKrantiHead from '../components/TechKrantiHead'
+
+export default function TechKranti() {
+    return (
+        <div className='min-h-[80vh]'>
+            <TechKrantiHead />
+            <AboutTechkranti />
+        </div>
+    )
+}
