62e6010aafb75861fb7dae9f4280d36f004b6a6b
Somen Samanta <111171599+S0M3N@users.noreply.github.com>
2023-08-24 14:18:09 +0530

    Add files via upload

---
+++ src/data/certificates.js
@@ -0,0 +1,22 @@
+export const certs = [
+  {
+    id: "001_shivangi_marketing_june01",
+    name: "Miss Shivangi Kashyap",
+    field: "Marketting",
+    grade: "A",
+    from: "June 1, 2023",
+    to: "August 30, 2023",
+    attribute1: "her",
+    attribute2: "her",
+  },
+  {
+    id: "002_somen_webdev_june01",
+    name: "Master Somen Samanta",
+    field: "Web Development",
+    grade: "A",
+    from: "June 1, 2023",
+    to: "August 30, 2023",
+    attribute1: "him",
+    attribute2: "his",
+  },
+];
