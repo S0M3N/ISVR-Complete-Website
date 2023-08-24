9fb160ef6eca34879c3c7205ae1fce8b11a24132
Somen Samanta <111171599+S0M3N@users.noreply.github.com>
2023-08-24 14:26:37 +0530

    Update certificates.js

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -2,7 +2,7 @@ export const certs = [
   {
     id: "001_shivangi_marketing_june01",
     name: "Miss Shivangi Kashyap",
-    field: "Marketting",
+    field: "Marketting Intern",
     grade: "A",
     from: "June 1, 2023",
     to: "August 30, 2023",
