e517a82c6689e2ab5f032a8a18766fa3f43be410
Somen Samanta <111171599+S0M3N@users.noreply.github.com>
2023-08-24 14:19:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-import Home from './pages/Home';
-import NotFound404 from './pages/NotFound404';
+import Home from "./pages/Home";
+import NotFound404 from "./pages/NotFound404";
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import About from './pages/About';
-import Services from './pages/Services';
-import Service from './pages/Service';
-import Gallery from './pages/Gallery';
-import TechKranti from './pages/TechKranti';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Service from "./pages/Service";
+import Gallery from "./pages/Gallery";
+import Cert from "./pages/Cert";
+import TechKranti from "./pages/TechKranti";
 
 const Layout = () => {
   return (
@@ -20,28 +21,31 @@ const Layout = () => {
       <Outlet />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 const router = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      { path: '/', element: <Home /> },
-      { path: '/about', element: <About /> },
-      { path: '/gallery', element: <Gallery /> },
-      { path: '/services', element: <Services /> },
-      { path: '/TechKranti', element: <TechKranti /> },
-      { path: '/service/:id', element: <Service /> },
-      { path: '*', element: <NotFound404 /> }
-    ]
-  }
-])
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      { path: "/gallery", element: <Gallery /> },
+      { path: "/services", element: <Services /> },
+      { path: "/TechKranti", element: <TechKranti /> },
+      { path: "/service/:id", element: <Service /> },
+      { path: "/cert/:id", element: <Cert /> },
+      { path: "*", element: <NotFound404 /> },
+    ],
+  },
+]);
 
 const App = () => {
-  return <div>
-    <RouterProvider router={router} />
-  </div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
