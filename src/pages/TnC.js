85dc145f35d1beae7334097341474a8513cc2558
Somen Samanta <111171599+S0M3N@users.noreply.github.com>
2023-08-30 21:31:11 +0530

    Add files via upload

---
+++ src/pages/TnC.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+export default function TnC() {
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-start pt-5 lg:min-h-[70vh] md:min-h-[75vh] min-h-[60vh]">
+      <h2 className="lg:text-3xl text-xl mb-5 font-semibold">
+        Terms and Conditions
+      </h2>
+      <div className="flex flex-col mb-8">
+        <p>
+          Welcome to the IndoplanetX Space Vault Space and Research website (the
+          "Website"). By using this Website, you agree to comply with and be
+          bound by the following Terms and Conditions. If you do not agree to
+          these terms, please refrain from using our Website.
+        </p>
+        <p className="flex flex-col gap-4 mt-5">
+          <b>1. Use of Content:</b>
+          The content on this Website, including text, images, graphics, and
+          other materials ("Content"), is for informational and educational
+          purposes only. You may not reproduce, distribute, modify, transmit,
+          reuse, or use the Content for any commercial purposes without our
+          prior written consent.
+          <b>2. Intellectual Property: </b>
+          All Content on this Website is the property of IndoplanetX Space Vault
+          and Research Pvt. Ltd. and is protected by intellectual property laws.
+          You may not use, reproduce, or distribute any logos, trademarks, or
+          service marks displayed on this Website without our permission.
+          <b>3. Internship and Collaboration:</b>
+          Any applications, submissions, or inquiries related to internships,
+          partnerships, or collaborations sent through this Website are subject
+          to our review and evaluation. We reserve the right to decline or
+          accept any application at our discretion.
+          <b>4. Disclaimer: </b>
+          The Content on this Website is provided "as is" without any
+          warranties, expressed or implied. IndoplanetX Space Vault and Research
+          Pvt. Ltd. makes no representations or warranties about the accuracy,
+          completeness, or suitability of the Content for any purpose.
+          <b>5. Limitation of Liability: </b>
+          IndoplanetX Space Vault and Research Pvt. Ltd. shall not be liable for
+          any direct, indirect, incidental, consequential, or punitive damages
+          arising out of your access to or use of this Website. Your use of the
+          Website is at your own risk.
+          <b>6. Links to Third-Party Websites: </b>
+          This Website may contain links to third-party websites for your
+          convenience. We do not endorse or control the content of these
+          third-party sites, and your use of them is subject to their respective
+          terms and conditions.
+          <b>7. Privacy Policy: </b>
+          Your use of this Website is also governed by our Privacy Policy, which
+          can be found on our Website. Please review our Privacy Policy to
+          understand how we collect, use, and protect your personal information.
+          <b>8. Changes to Terms and Conditions: </b>
+          We reserve the right to update or modify these Terms and Conditions at
+          any time without prior notice. Your continued use of the Website after
+          any changes indicates your acceptance of the modified terms.
+          <b>9. Governing Law:</b>
+          These Terms and Conditions shall be governed by and construed in
+          accordance with the laws of INDIA, without regard to its conflict of
+          law principles.
+          <b>10. Contact Us: </b>
+          <p>
+            If you have any questions or concerns regarding these Terms and
+            Conditions, please contact us at{" "}
+            <a href="mailto:info@indoplanetxsvr.com" className="text-blue-400">
+              info@indoplanetxsvr.com
+            </a>
+            . By using this Website, you acknowledge that you have read,
+            understood, and agree to these Terms and Conditions. Please note
+            that this is a general template and should be customized to fit the
+            specific needs and legal requirements of your website and
+            jurisdiction. It's advisable to consult with a legal professional to
+            ensure that your Terms and Conditions accurately reflect your
+            business practices and comply with relevant laws.
+          </p>
+        </p>
+      </div>
+    </div>
+  );
+}
