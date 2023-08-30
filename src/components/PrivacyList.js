cd74dd3207b065a148233ac233e1f96a975f1666
Somen Samanta <111171599+S0M3N@users.noreply.github.com>
2023-08-30 21:25:36 +0530

    Add files via upload

---
+++ src/components/PrivacyList.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+export default function PrivacyList() {
+  return (
+    <div className="items-start mb-4">
+      <h4 className="text-xl font-bold">1. Information We Collect:</h4>
+      <h6 className="text-lg font-semibold mb-2 mt-4">
+        1.1. Personal Information:
+      </h6>
+      <p>
+        We may collect personal information, such as your name, email address,
+        phone number, and other contact details when you submit forms or contact
+        us through the Website.
+      </p>
+      <h6 className="text-lg font-semibold mb-2 mt-4">
+        1.2. Usage Information:
+      </h6>
+      <p>
+        We may collect non-personal information about how you use and navigate
+        our Website. This includes information about your device, browser, IP
+        address, and the pages you visit on our Website.
+      </p>
+      <h4 className="text-xl font-bold mt-5">
+        2. How We Use Your Information:
+      </h4>
+      <h6 className="text-lg font-semibold mb-2 mt-4">
+        2. How We Use Your Information:
+      </h6>
+      <p>
+        We may use the information you provide to respond to your inquiries,
+        process applications for internships and collaborations, and provide you
+        with information about our services.
+      </p>
+      <h6 className="text-lg font-semibold mb-2 mt-4">2.2. Analytics:</h6>
+      <p>
+        We may use usage information for analytical purposes to improve the
+        functionality and content of our Website.
+      </p>
+      <h6 className="text-lg font-semibold mb-2 mt-4">2.3. Communication:</h6>
+      <p>
+        We may use your contact information to send you updates, newsletters,
+        and promotional materials related to our services. You can opt-out of
+        receiving such communications at any time.
+      </p>
+      <h4 className="text-xl font-bold mt-5">3. Sharing Your Information:</h4>
+      <p>
+        We do not sell, trade, or rent your personal information to third
+        parties. However, we may share your information with trusted third-party
+        service providers who assist us in operating our Website and providing
+        services to you. We ensure that these third parties adhere to strict
+        confidentiality and security standards.
+      </p>
+      <h4 className="text-xl font-bold mt-5">
+        4. Cookies and Tracking Technologies:
+      </h4>
+      <p>
+        Our Website may use cookies and similar tracking technologies to enhance
+        your experience and collect usage information. You can control the use
+        of cookies through your browser settings.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/PrivacyTop.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function PrivacyTop() {
+  return (
+    <div className="flex flex-col">
+      <div className="lg:text-3xl text-2xl font-semibold">Privacy Policy</div>
+      <p className="text-justify my-4">
+        This Privacy Policy describes how IndoplanetX Space Pvt. Ltd.
+        ("IndoplanetX," "we," "us," or "our") collects, uses, and protects the
+        personal information you provide to us through the IndoplanetX Space
+        Vault Space and Research website (the "Website"). By using this Website,
+        you agree to the terms of this Privacy Policy.
+      </p>
+    </div>
+  );
+}
