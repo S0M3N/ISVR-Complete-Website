c3b8ef79847c6f05d540e1376a8048d4fedf197a
Somen Samanta <111171599+S0M3N@users.noreply.github.com>
2023-08-31 08:15:22 +0530

    Add files via upload

---
+++ src/components/DisclaimerLink.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+export default function DisclaimerLink() {
+  return (
+    <div className="items-start mb-4 text-justify mt-8">
+      <h4 className="text-xl font-bold">1. General Information:</h4>
+      <p>
+        The content provided on this Website is for informational and
+        educational purposes only. It is not intended to provide professional
+        advice, including but not limited to legal, financial, or technical
+        advice. Any reliance on the information provided on this Website is at
+        your own risk.
+      </p>
+      <h4 className="text-xl font-bold mt-4">2. Accuracy and Completeness:</h4>
+      <p>
+        While we strive to provide accurate and up-to-date information, we do
+        not make any warranties or representations regarding the accuracy,
+        completeness, or reliability of the content on this Website. We reserve
+        the right to modify, update, or remove content without prior notice.
+      </p>
+      <h4 className="text-xl font-bold mt-4">3. Use of Information:</h4>
+      <p>
+        The use of any information or materials on this Website is solely at
+        your own discretion and risk. It shall be your responsibility to ensure
+        that any products, services, or information available through this
+        Website meet your specific requirements.
+      </p>
+      <h4 className="text-xl font-bold mt-4">4. Third-Party Content:</h4>
+      <p>
+        This Website may include links to third-party websites or content. These
+        links are provided for your convenience and do not signify our
+        endorsement of such websites or content. We have no control over the
+        nature, content, and availability of these external sites.
+      </p>
+      <h4 className="text-xl font-bold mt-4">5. Limitation of Liability:</h4>
+      <p>
+        IndoplanetX Space Pvt. Ltd., its employees, affiliates, and partners
+        shall not be liable for any direct, indirect, incidental, consequential,
+        or punitive damages arising from your access to or use of this Website.
+        This includes but is not limited to any errors, omissions, inaccuracies,
+        or delays in the content.
+      </p>
+      <h4 className="text-xl font-bold mt-4">6. Changes to Disclaimer:</h4>
+      <p>
+        We reserve the right to update or modify this Disclaimer at any time
+        without prior notice. Your continued use of the Website after any
+        changes indicates your acceptance of the modified terms.
+      </p>
+      <h4 className="text-xl font-bold mt-4">7. Contact Us:</h4>
+      <p>
+        If you have any questions or concerns about this Disclaimer, please
+        contact us at{" "}
+        <a href="mailto:info@indoplanetxsvr.com" className="text-blue-500">
+          info@indoplanetxsvr.com
+        </a>
+      </p>
+      <h4 className="text-xl font-bold mt-4">8. Governing Law:</h4>
+      <p>
+        This Disclaimer shall be governed by and construed in accordance with
+        the laws of INDIA, without regard to its conflict of law principles.{" "}
+        <br />
+        <br />
+        By using this Website, you acknowledge that you have read, understood,
+        and agree to this Disclaimer.
+      </p>
+    </div>
+  );
+}
---
+++ src/components/DisclaimerTop.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function DisclaimerTop() {
+  return (
+    <div className="flex flex-col mt-4">
+      <div className="lg:text-3xl text-2xl font-semibold">Disclaimer</div>
+      <p className="text-justify my-4">
+        Welcome to the Disclaimer page of IndoplanetX Space Vault Space and
+        Research website (the "Website"). By using this Website, you acknowledge
+        and agree to the terms outlined in this disclaimer. If you do not agree
+        with these terms, please refrain from using our Website.
+      </p>
+    </div>
+  );
+}
