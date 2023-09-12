a2c79725793076a0bfce46a58c86f319a770d78c
Somen Samanta <111171599+S0M3N@users.noreply.github.com>
2023-09-12 21:46:00 +0530

    Update MissionVision.js

--- src/components/MissionVision.js
+++ src/components/MissionVision.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React from "react";
 
 export default function MissionVision() {
-    return (
-        <div className='w-full bg-zinc-800 pb-8 pt-1'>
-            <div className='container mx-auto flex flex-col items-center justify-center mt-5'>
-                <div className='text-center lg:text-3xl text-xl font-bold'>Our Vision <span className='text-green-500'>and Mission</span></div>
-                <div className='border border-white lg:w-[20vw] w-[40vw] lg:my-2 my-1'></div>
-                <div className='text-center lg:text-lg'>
-                    Our vision for the global student community is to create an open learning platform that is accessible to everyone. At ISVR, we offer applied technology and project-based learning, enabling students to inquire, collaborate, create, and learn while tackling new problems and refining their solutions. Our platform is designed to provide Space Awareness Activities, Educational Programs, and Research & Development of Space Exploration Technologies.
-                    <br /><br />
-                    One of our key offerings is providing internships to students, where we aim to ignite their young minds. We offer internships in various fields such as Rocketry, Satellites & Communications, Drone technologies, Space medicine, Astronomy, Space research, and Space rover. Through these internships, students get hands-on experience and exposure to the exciting world of space.
-                    <br /><br />
-                    At ISVR, we are actively involved in space research and development. Currently, our focus is on developing a reusable rocket with open fearing technology. A reusable launch vehicle is designed with components that can be recovered and reflown, allowing for multiple uses while carrying payloads from the surface to outer space. Rocket stages are the primary parts of the launch vehicle that we aim to reuse, and we are also working on SS motors to enhance our capabilities.
-                </div>
-            </div>
+  return (
+    <div className="w-full bg-zinc-800 pb-8 pt-1">
+      <div className="container mx-auto flex flex-col items-center justify-center mt-5">
+        <div className="text-center lg:text-3xl text-xl font-bold">
+          Our Vision <span className="text-green-500">and Mission</span>
         </div>
-    )
+        <div className="border border-white lg:w-[20vw] w-[40vw] lg:my-2 my-1"></div>
+        <div className="text-justify lg:text-lg">
+          Our vision for the global student community is to create an open
+          learning platform that is accessible to everyone. At ISVR, we offer
+          applied technology and project-based learning, enabling students to
+          inquire, collaborate, create, and learn while tackling new problems
+          and refining their solutions. Our platform is designed to provide
+          Space Awareness Activities, Educational Programs, and Research &
+          Development of Space Exploration Technologies.
+          <br />
+          <br />
+          One of our key offerings is providing internships to students, where
+          we aim to ignite their young minds. We offer internships in various
+          fields such as Rocketry, Satellites & Communications, Drone
+          technologies, Space medicine, Astronomy, Space research, and Space
+          rover. Through these internships, students get hands-on experience and
+          exposure to the exciting world of space.
+          <br />
+          <br />
+          At ISVR, we are actively involved in space research and development.
+          Currently, our focus is on developing a reusable rocket with open
+          fearing technology. A reusable launch vehicle is designed with
+          components that can be recovered and reflown, allowing for multiple
+          uses while carrying payloads from the surface to outer space. Rocket
+          stages are the primary parts of the launch vehicle that we aim to
+          reuse, and we are also working on SS motors to enhance our
+          capabilities.
+        </div>
+      </div>
+    </div>
+  );
 }
